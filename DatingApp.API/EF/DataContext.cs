f435966f26b0ed462a4b2694c900f8aeea29d8fc
M. Ahsan Jutt <ahsanjutt0999@outlook.com>
2019-03-13 18:01:43 +0500

    add user and Iauthrepositry

--- DatingApp.API/EF/DataContext.cs
+++ DatingApp.API/EF/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.EF
         public DataContext(DbContextOptions<DataContext> options):base(options) {}
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
